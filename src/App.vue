0acfde6dcfe2416b04ba042d09f0ad0bb8038796
TELstatic <telstatic@gmail.com>
2019-01-16 10:41:48 +0800

    修复 v-model 数据不同步 BUG

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,39 @@
 <template>
     <div id="app">
         <div>
-            <x-media v-model="images3"
-                     :urls="urls"
-                     :max="1"
-                     type="object"
-                     :config="config"
-                     @callback="callback">
-            </x-media>
+
+            <Button type="success" @click="handleCreate">新增</Button>
+            <Modal :transfer="false"
+                   :z-index="1"
+                   v-model="visable"
+                   title="品牌">
+                <Form ref="form" :model="form" :label-width="80">
+                    <FormItem label="名称" prop="title">
+                        <Input v-model="form.title" size="large" placeholder="请输入名称"></Input>
+                    </FormItem>
+                    <FormItem label="排序" prop="sort">
+                        <Tooltip content="品牌排序,值越小越靠前" placement="right">
+                            <Icon type="help-circled"></Icon>
+                        </Tooltip>
+                        <InputNumber :max="255" :min="0" v-model="form.sort"></InputNumber>
+                    </FormItem>
+                    <FormItem label="商标" prop="images">
+                        <x-media v-model="form.images"
+                                 :urls="urls"
+                                 :max="1"
+                                 type="string"
+                                 :config="config"
+                                 @callback="callback">
+                        </x-media>
+                    </FormItem>
+                </Form>
+                <div slot="footer">
+                    <Button type="primary" v-if="!form.id" @click="handleSubmit">保存</Button>
+                    <Button type="warning" v-else @click="handleSubmit">更新</Button>
+                    <Button @click="handleReset" style="margin-left: 8px">重置</Button>
+                </div>
+            </Modal>
+
         </div>
     </div>
 </template>
@@ -18,6 +44,13 @@
         name: 'app',
         data() {
             return {
+                visable: false,
+                form: {
+                    id: null,
+                    title: null,
+                    sort: 0,
+                    images: ''
+                },
                 urls: {
                     index: 'test.json',    //获取文件地址
                     upload: '',   //上传地址
@@ -39,11 +72,11 @@
                 max: 1,
                 random: true,
                 id: null,
-                images1: '',
+                images1: '//delii.oss-cn-shanghai.aliyuncs.com/rakan/default/NJGDZQ/test/Paris.jpg',
                 images2: [],
                 images3: [
                     {
-                        url:'//sds'
+                        url: '//sds'
                     }
                 ],
                 msg: 'Welcome to Your Vue.js App'
@@ -52,6 +85,24 @@
         methods: {
             callback(val) {
                 console.log(val)
+            },
+            handleCreate() {
+                this.clear();
+                this.visable = true;
+            },
+            handleSubmit() {
+                console.log(this.form)
+            },
+            handleReset() {
+
+            },
+            clear() {
+                this.form = {
+                    id: null,
+                    title: null,
+                    sort: 0,
+                    images: ''
+                };
             }
         }
     }
